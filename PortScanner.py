53577c579211e4637ed1c394d9a5e842f2f3edf7
Chris-Lenz <65963578+Chris-Lenz@users.noreply.github.com>
2020-08-16 10:43:10 -0400

    Add files via upload
    Next Step, output to CSV file, working on looping it with typing quit to exit, not quite there.

--- PortScanner.py
+++ PortScanner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """1. On execution of program system should prompt “Enter a host to scan”. User will provide a host name
 2. System should look for all the ports between the range of 1 to 1025
 3. If the Ports is open it should create a file and add an entry for port number
@@ -17,72 +19,82 @@ from queue import Queue
 socket.setdefaulttimeout(0.25)
 print_lock = threading.Lock()
 
-# input hostname or IP, try again on error
-while True:
-    try:
-        target = input('Enter the host to be scanned: ')
-        target_file = str(target + ".txt")
-        t_IP = socket.gethostbyname(target)
-        break
-    except:
-        bad_target = ("bad_target" + target_file)
-        with open(bad_target, 'w') as file:
-            file.write(str(target) + "does not exist\n")
-        print(str(target) + " does not exist, please try again")
+target = ""
 
+target_file = "results.csv"
 with open(target_file, "a") as file:
-    file.write("Scanning: " + target + "\n\n")
-print ('Starting scan on host: ', t_IP)
+    file.write("Host Name,Scanned,Open Ports,Start Time,End Time,Elapsed Time, \n")
 
-def portscan(port):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            con = s.connect((t_IP, port))
-            with print_lock:
-                with open(target_file, "a") as file:
-                    print(port, 'is open')
-                    file.write("Port " + str(port) + " is open\n")
-                    con.close()
-        except:
-            pass
+while target.lower() != "quit":
+    print(target)
 
-def threader():
+
+
+    # input hostname or IP, try again on error
     while True:
-        worker = q.get()
-        portscan(worker)
-        q.task_done()
+        try:
+            target = input('Enter the host to be scanned (type quit to exit): ')
+            t_IP = socket.gethostbyname(target)
+            break
+        except:
+            with open(target_file, 'a') as file:
+                file.write(target + "," + "NO\n")
 
-q = Queue()
-startTime = time.time()
-date = str(datetime.now())
+            print(str(target) + " does not exist, please try again")
+
+    with open(target_file, "a") as file:
+        file.write(target + ",YES,")
+    print ('Starting scan on host: ', t_IP)
+
+    def portscan(port):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                con = s.connect((t_IP, port))
+                with print_lock:
+                    with open(target_file, "a") as file:
+                        print(port, 'is open')
+                        file.write("Port " + str(port) + " ")
+                        con.close()
+            except:
+                pass
+
+    def threader():
+        while True:
+            worker = q.get()
+            portscan(worker)
+            q.task_done()
+
+    q = Queue()
+    startTime = time.time()
+    date = str(datetime.now())
 
 
-for x in range(100):
-    t = threading.Thread(target = threader)
-    t.daemon = True
-    t.start()
-for worker in range(1, 1025):
-    q.put(worker)
-q.join()
+    for x in range(100):
+        t = threading.Thread(target = threader)
+        t.daemon = True
+        t.start()
+    for worker in range(1, 1025):
+        q.put(worker)
+    q.join()
 
-endtime = str(datetime.now())
-
-
+    endtime = str(datetime.now())
 
 
-print(target + " scan complete")
-start_time = ("Start time: {:.19}".format(date))
-end_time = ("End time:   {:.19}".format(endtime))
-elapsed_time = ('Time taken: {:.2f} seconds'.format(time.time() - startTime))
 
-print(start_time)
-print(end_time)
-print(elapsed_time)
 
-with open(target_file, "a") as file:
-    file.write("\n" + start_time)
-    file.write("\n" + end_time)
-    file.write("\n" + elapsed_time + "\n\n")
+    print(target + " scan complete")
+    start_time = ("{:.19}".format(date))
+    end_time = ("{:.19}".format(endtime))
+    elapsed_time = ("{:.2f} seconds".format(time.time() - startTime))
+
+    print(start_time)
+    print(end_time)
+    print(elapsed_time)
+
+    with open(target_file, "a") as file:
+        file.write("," + start_time)
+        file.write("," + end_time)
+        file.write("," + elapsed_time + "\n")
 
 
 
